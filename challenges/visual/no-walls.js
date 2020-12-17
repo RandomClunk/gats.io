aa1906fd08bd90eaf1acbafc0ff8aec460fe2f53
RandomClunk <74115138+RandomClunk@users.noreply.github.com>
2020-12-17 14:50:22 -0500

    Update no-walls.js

--- challenges/visual/no-walls.js
+++ challenges/visual/no-walls.js
@@ -1,8 +1,2 @@
-fetch('https://gats.io/js/1.7.15.js') // request to get the current client source code
-  .then(res => {
-    // status 404 means not found
-    if(res.status === 404) return alert('This challenge was made for 1.7.15 but the game has updated.');
-    
-    // patch the function that activates walls to simply an empty function
-    RB.prototype.activate = () => {};
-  });
+// patch the function that activates walls to simply an empty function
+RB.prototype.activate = () => {};
