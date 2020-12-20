900e0cbb24e3d49074acfa40ee4d6a848400cb3d
RandomClunk <74115138+RandomClunk@users.noreply.github.com>
2020-12-19 19:34:39 -0500

    Create debugger-bypass.js

---
+++ patches/debugger-bypass.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         gats.io debugger bypass
+// @match        gats.io
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+window.addEventListener('load', () => {
+  _0x115e[_0x115e.indexOf('debu')] = '';
+});
