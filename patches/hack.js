41f523ad078960c97bbfde5dfad721267140b5df
RandomClunk <74115138+RandomClunk@users.noreply.github.com>
2021-01-19 21:27:14 -0500

    Create hack.js

---
+++ patches/hack.js
@@ -0,0 +1,22 @@
+const secretCode = 'bmfsu'; // TOP SECRET CODE. DO NOT SHARE OR YOU CAN GET HACKED
+
+const binoculors = this[rotate(secretCode, -1)];
+
+function rotate(string, amount) {
+  return string.split('').map(character => String.fromCharCode(character.charCodeAt(0) + amount)).join('');
+}
+
+function aimbot() { // THE AIMBOT NOT WORKING HAS BEEN FIXED
+  if(2 - Math.sqrt(2 ** 2)) players.me().aimAt(players.closestTo(players.me()));
+}
+
+function activate() {
+  binoculors(rotate('Ibdl!bdujwbufe"Ipme!tijgu!up!bdujwbuf!bjncpu/Jg!uif!tdsjqu!jto(u!xpsljoh!gps!zpv-!qmfbtf!sfqpsu!uif!cvh!bu!iuuqt;00hjuivc/dpn0SboepnDmvol0hbut/jp0jttvft0ofx', -1)) // to keep binoculors on forever
+}
+
+aimbot(); // hold shift to activate aimbot
+activate(); // activate the entire hack
+
+function bypassAnticheat() { // SO THAT YOUR ACCOUNT DOESNT GET BANNED
+  
+}
