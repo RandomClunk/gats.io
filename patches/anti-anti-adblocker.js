082a17c0ba7f480f223a69bcfb1f5c258506cc8a
RandomClunk <74115138+RandomClunk@users.noreply.github.com>
2021-01-19 21:30:36 -0500

    Create anti-anti-adblocker.js

---
+++ patches/anti-anti-adblocker.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Anti anti adblocker
+// @match       gats.io
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+window.addEventListener('load', () => {
+  window.document.body.appendChild(document.createElement('div')).id = 'a6d2b08fad49';
+});
