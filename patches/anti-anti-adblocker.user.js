d0e9890a2fc3080341fa933f199b2cd60b4de4ee
RandomClunk <74115138+RandomClunk@users.noreply.github.com>
2021-04-20 09:58:52 -0400

    Update for 2.0.4

--- patches/anti-anti-adblocker.user.js
+++ patches/anti-anti-adblocker.user.js
@@ -5,6 +5,14 @@
 // @grant       none
 // ==/UserScript==
 
-window.addEventListener('load', () => {
-  window.document.body.appendChild(document.createElement('div')).id = 'a6d2b08fad49';
-});
+// store reference to the original `getComputedStyle` function
+const originalGetComputedStyle = window.getComputedStyle;
+
+// replace the global function with a proxy
+window.getComputedStyle = function(e) {
+  // if the id of the element passed in is `detect` (id of the element used to detect adblockers), then return an object with a display property; can be set to anything really
+  if (e.id === 'detect') return { display: 0 };
+  
+  // do what the original `getComputedStyle` would have done
+  return originalGetComputedStyle.apply(null, arguments);
+};
